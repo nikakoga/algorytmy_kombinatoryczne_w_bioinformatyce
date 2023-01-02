912ad83f50ae20d56b0b879b7f1c65f695e9bbb6
Norbert Litkowski <norbert.litkowski@gmail.com>
2023-01-02 23:13:08 +0100

    finding stars

---
+++ norbert_helps_nikusia/script.py
@@ -0,0 +1,43 @@
+from itertools import combinations
+
+
+def main():
+    vertices = get_vertices()
+    stars = set()
+    for v, neighbours in vertices.items():
+        res = get_star(v, vertices, neighbours)
+        if res is not None:
+            stars.add(res)
+    print(stars)
+
+
+def get_star(verticle, all_vertices, neighbours) -> tuple | None:
+    for c in combinations(neighbours, r=4):
+        d = dict.fromkeys(c, 1)
+        d[verticle] = 1
+        for n in c:
+            if n in all_vertices.keys():
+                d[n] = d[n] + 1
+                for nn in all_vertices[n]:
+                    if nn in d.keys():
+                        d[nn] = d[nn] + 1
+        for times_present in d.values():
+            if times_present < 5:
+                return None
+        return tuple(sorted(d.keys()))
+
+
+def get_vertices() -> dict:
+    res = {}
+    with open(r"C:\Users\norbert\code\akwb\norbert_helps_nikusia\input.txt", "r") as f:
+        for l in f.readlines():
+            tmp = l.split('->')
+            neighbours = list(
+                map(lambda x: int(x.strip()), filter(lambda x: x != tmp[0], tmp[1].split())))
+            if len(neighbours) >= 4:
+                res[int(tmp[0])] = neighbours
+    return res
+
+
+if __name__ == "__main__":
+    main()
